94237d1ffb8b10c73a83a30cac992c4d700fdcb9
John Otander <johnotander@gmail.com>
2018-06-27 17:19:49 -0600

    Monorepo all the things

---
+++ nick/constants.js
@@ -0,0 +1,20 @@
+export const stuff = [
+  'Blue Bottle pour over single cupper, made in Japan',
+  'Coffee: 25g dose. 5 rounds of 70g of water, with 30 seconds in between.',
+  'Beep boop music',
+  'Sous vide'
+]
+
+export const css = `
+body { margin: 0; }
+.Typist .Cursor { display: inline-block; }
+.Typist .Cursor--blinking {
+  opacity: 1;
+  animation: blink 1s linear infinite;
+}
+@keyframes blink {
+  0% { opacity: 1; }
+  50% { opacity: 0; }
+  100% { opacity: 1; }
+}
+`
